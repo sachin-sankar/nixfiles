0e898da67e9df048e9cf7582f8ba407f3543a0d1
Sachin Sankar <mail.sachinsankar@gmail.com>
2026-08-11 23:45:13 +0530

    Sync changes\

--- dotfiles/config/hypr/hyprland.lua
+++ dotfiles/config/hypr/hyprland.lua
@@ -19,7 +19,6 @@ hl.on("hyprland.start", function()
 	hl.exec_cmd("wl-paste --watch cliphist store")
 	hl.exec_cmd("/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1")
 	hl.exec_cmd("noctalia")
-	hl.exec_cmd("wall")
 end)
 
 -----------------------
